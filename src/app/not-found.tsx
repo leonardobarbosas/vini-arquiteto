a7b6a0e727270bda89d6db2fdc601fe875148cc6
leonardobarbosas <leonardobarbosasantos@gmail.com>
2026-05-25 21:00:23 -0300

    criando fundação do projeto

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="div-page-not-found flex flex-col justify-center items-center gap-4">
+      <h1 className="h1-page-not-found md:text-custom-yellow font-bold text-xl">
+        Página não encontrada. Acesse outra página!
+      </h1>
+    </div>
+  );
+}
